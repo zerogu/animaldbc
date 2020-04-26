993dbaee8a16589a252309d0a3d42e78e627d994
guwenjia <guwenjia@bilibili.com>
2020-04-26 14:37:25 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,6 @@ const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
 
 module.exports = {
   // baseUrl: process.env.NODE_ENV === 'production' ? '/app/' : '/',
-  outputDir: 'docs',
   runtimeCompiler: true,
   devServer: {
     host: '127.0.0.1',
